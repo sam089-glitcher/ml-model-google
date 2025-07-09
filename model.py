50173dc6c476328bbbfe2a45516c8e9bacc0ead4
sam089-glitcher <saumitram97@gmail.com>
2025-07-10 03:47:50 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import logging
+import google.cloud.logging as cloud_logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+from google.cloud.logging_v2.handlers import setup_logging
+
+cloud_logger = logging.getLogger('cloudLogger')
+cloud_logger.setLevel(logging.INFO)
+cloud_logger.addHandler(CloudLoggingHandler(cloud_logging.Client()))
+cloud_logger.addHandler(logging.StreamHandler())
+
+
+# In[2]:
+
+
+# Import TensorFlow
+import tensorflow as tf
+
+
+# In[3]:
+
+
+# Import numpy
+import numpy as np
+
+
+# In[4]:
+
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+
+# In[7]:
+
+
+model = tf.keras.Sequential([
+    tf.keras.Input(shape=(1,)),
+    tf.keras.layers.Dense(units=1)
+])
+
+
+# In[8]:
+
+
+model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.MeanSquaredError())
+
+
+# In[9]:
+
+
+model.fit(xs, ys, epochs=500)
+
+
+# In[10]:
+
+
+import logging
+import google.cloud.logging as cloud_logging
+from google.cloud.logging.handlers import CloudLoggingHandler
+from google.cloud.logging_v2.handlers import setup_logging
+
+cloud_logger = logging.getLogger('cloudLogger')
+cloud_logger.setLevel(logging.INFO)
+cloud_logger.addHandler(CloudLoggingHandler(cloud_logging.Client()))
+cloud_logger.addHandler(logging.StreamHandler())
+
+import tensorflow as tf
+import numpy as np
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+model = tf.keras.Sequential([tf.keras.layers.Dense(units=1, input_shape=[1])])
+
+model.compile(optimizer=tf.keras.optimizers.SGD(), loss=tf.keras.losses.MeanSquaredError())
+
+model.fit(xs, ys, epochs=500)
+
+
+# In[11]:
+
+
+cloud_logger.info(str(model.predict(np.array([10.0]))))
+
+
+# In[ ]:
+
+
+
+
